932839cbcca0f8df7f5381ab40edebd2014dbb8f
bjick84 <bjick@web.de>
2023-01-16 14:35:03 +0100

    Fallunterscheidung / SWITCH|CASE 1

--- script_01.js
+++ script_01.js
@@ -2,23 +2,23 @@
 
 // Entscheidungsstrukturen | control flow
 
-// Deklaration + Assigment
-const ageJohn = 35;
-const ageMark = 30;
+// // Deklaration + Assigment
+// const ageJohn = 35;
+// const ageMark = 30;
 
-// Deklaration
-let isJohnOlder, isJohnEqual;
+// // Deklaration
+// let isJohnOlder, isJohnEqual;
 
-// Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark); // = * 1 ist Zuweiser == * 2 Test auf Gleichheit
+// // Test | Logische Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark); // = * 1 ist Zuweiser == * 2 Test auf Gleichheit
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -49,3 +49,36 @@ console.log("------------------");
 
 /************  Ternäre (ternary) Schreibweise ************/
 // console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+ 
+const firstName = "Jane";
+let job;
+ 
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein! 
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+// job = "instructor";  // .. unterrichtet!
+
+switch (job) 
+{
+    case "driver":
+        console.log("Jane fährt Taxi / Uber")
+        break;
+    case "diver":
+        console.log("Jane taucht im Rhein!")
+        break;
+    case "artist":
+    console.log("Jane malt ein Bild!")
+        break;
+    case "teacher":
+    case "instructor":
+    console.log("Jane unterrichtet")
+        break;   
+
+    default:
+        console.log("Jane macht etwas anderes!")
+        break;
+}
